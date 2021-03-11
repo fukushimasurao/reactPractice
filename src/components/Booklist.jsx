f215aa556663167ebdb569b7fae0a01bd3743a90
fukushima hayao <nu0640042@gmail.con>
2021-03-12 01:03:35 +0900

    first commit

---
+++ src/components/Booklist.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+const Booklist = props => {
+    const [bookData, setBookData] = useState(null);
+
+    useEffect(() => {
+        const result = props
+            .getData?.(props.language)
+            .then((response) => setBookData(response));
+    }, [props]);
+    return (
+        <div>
+            <ul>
+                {
+                    // このあたり編集
+                    bookData === null ? (
+                        <p>now loading...</p>
+                    ) : (
+                        bookData.data.items.map((x, index) => (
+                            <li key={index}>{x.volumeInfo.title}</li>
+                        ))
+                    )
+                }
+            </ul>
+        </div>
+    );
+}
+
+export default Booklist;
